933d06c027044083874ce8da9944dae16298451c
dvveera <d.v.veera@gmail.com>
2018-11-30 22:50:42 -0500

    Inital commit

---
+++ build/CMakeFiles/cv_basic.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cv_basic.dir/cv-basic.cpp.o"
+  "cv_basic.pdb"
+  "cv_basic"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cv_basic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/cv_face.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cv_face.dir/cv-face.cpp.o"
+  "cv_face.pdb"
+  "cv_face"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cv_face.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
